86c6ec7b8ba60b53fece55d9b9583d7c2678308b
Jackson Mohsenin <jmohsenin@gmail.com>
2013-07-23 16:12:10 -0700

    basic views in place with ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,28 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+var pocket = angular.module('pocket', ['pocket.filters', 'pocket.services', 'pocket.directives', 'pocket.controllers', 'ui.state']);
+
+
+pocket.config(function($stateProvider, $urlRouterProvider) {
+  //$locationProvider.html5Mode(true);
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state('index', {
+      url: '/home',
+      templateUrl: 'partials/list.html',
+      controller: 'ArticleList'
+    })
+    .state('index.detail', {
+      url: '/articles/:articleId',
+      templateUrl: 'partials/detail.html',
+      controller: 'ArticleDetail'
+    });
+
+  //$routeProvider
+  //  .when('/', {templateUrl: 'partials/list.html', controller: 'ArticleList'})
+  //  .when('/articles/:articleId', {templateUrl: 'partials/article-detail.html', controller: 'ArticleDetail'})
+  //  .otherwise({redirectTo: '/'});
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,19 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('pocket.controllers', []).
+  controller('ArticleList', function($scope) {
+  	$scope.articles = [
+	  	{'id': 'fomo',
+	  	'title': 'Do You Suffer from #FOMO?',
+	  	'url': 'http://www.gq.com/news-politics/mens-lives/201308/instagram-fomo-disorder-treatment-plan'},
+	  	{'id': 'walter-white',
+	  	'title': 'The Last Stand of Walter White',
+	  	'url': 'http://www.gq.com/entertainment/celebrities/201308/bryan-cranston-walter-white-breaking-bad-season-6'}, 
+	  	{'id': 'joe-biden',
+	  	'title': 'Have You Heard the One About President Joe Biden?',
+	  	'url': 'http://www.gq.com/news-politics/newsmakers/201308/joe-biden-presidential-campaign-2016-2013#ixzz2Zu3rrl'}
+	  	]
+  }).controller('ArticleDetail', function($scope, $routeParams) {
+  	$scope.phoneId = $routeParams.phoneId;
+  });
